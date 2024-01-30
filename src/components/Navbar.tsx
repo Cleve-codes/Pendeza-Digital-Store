22562b1c64d30e51ea6542c6e453e2922b9e458b
Cleve-codes <clevemomanyi@gmail.com>
2024-01-30 21:28:10 +0300

    cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,23 @@
 // import React from 'react'
-import Link from "next/link"
-import MaxWidthWrapper from "./MaxWidthWrapper"
-import { Icons } from "./Icons"
-import NavItems from "./NavItems"
-import { buttonVariants } from "./ui/button"
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Icons } from "./Icons";
+import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
-
   const user = null;
 
   return (
-    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16" >
-      <header className="relative bg-white" >
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+      <header className="relative bg-white">
         <MaxWidthWrapper>
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
-
               {/* Mobile Nav */}
-              <div className="ml-4 flex lg:ml-0" >
-                <Link href='/' >
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
                   <Icons.logo className="h-10 w-10" />
                 </Link>
               </div>
@@ -29,23 +28,51 @@ const Navbar = () => {
 
               <div className="ml-auto flex items-center">
                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                  {user ? null :
-                  <Link href="/sign-in" className={buttonVariants(
-                    {
-                      variant: 'ghost'
-                    }
-                  )} >
-                  Sign In
-                  </Link>}
+                  {user ? null : (
+                    <Link
+                      href="/sign-in"
+                      className={buttonVariants({
+                        variant: "ghost",
+                      })}
+                    >
+                      Sign In
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+
+                  {user ? (
+                    <p></p>
+                  ) : (
+                    <Link
+                      href="/sign-up"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Create Account
+                    </Link>
+                  )}
+
+                  {user ? <span className="h-6 w-px bg-gray-200" /> : null}
+
+                  {user ? null : (
+                    <div className="flex lg:ml-6">
+                      <span className="h-6 w-px bg-gray-200" />
+                    </div>
+                  )}
+
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
                 </div>
               </div>
-
             </div>
           </div>
         </MaxWidthWrapper>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function formatCurrency(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "  KSH",
+    notation?: Intl.NumberFormatOptions["notation"],
+  } = {}
+){
+  const { currency = 'USD', notation = 'compact' } = options
+
+  const numericPrice = typeof price === 'string' ? parseFloat(price) : price
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
+}
