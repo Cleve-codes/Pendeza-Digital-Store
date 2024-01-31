99fa57b3b48b79db9203e4e94464d784efa83bdb
Cleve-codes <clevemomanyi@gmail.com>
2024-01-31 10:02:31 +0300

    empty cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,31 +1,39 @@
-"use client"
-import { Link, ShoppingCart } from "lucide-react"
+"use client";
+import { ShoppingCart } from "lucide-react";
 // import React from 'react'
 
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger, SheetFooter } from "./ui/sheet"
-import { Separator } from "./ui/separator"
-import { formatCurrency } from "@/lib/utils"
-import { buttonVariants } from "./ui/button"
-import Image from "next/image"
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+  SheetFooter,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { formatCurrency } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 const Cart = () => {
-
-  const itemCount = 0
-  const fee = 1
+  const itemCount = 0;
+  const fee = 1;
 
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
-        <ShoppingCart className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
+        <ShoppingCart
+          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+          aria-hidden="true"
+        />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
           0
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle>
-          Cart (0)
-          </SheetTitle>
+          <SheetTitle>Cart (0)</SheetTitle>
         </SheetHeader>
         {itemCount > 0 ? (
           <>
@@ -50,38 +58,48 @@ const Cart = () => {
                 </div>
               </div>
 
-            <SheetFooter>
+              <SheetFooter>
                 <SheetTrigger asChild>
-                  <Link href="/cart" className={buttonVariants({
-                    className: 'w-full'
-                  })}>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({
+                      className: "w-full",
+                    })}
+                  >
                     Continue to Checkout
                   </Link>
                 </SheetTrigger>
-            </SheetFooter>
-
+              </SheetFooter>
             </div>
           </>
-        ) : (<div className="flex h-full flex-col items-center justify-center space-y-1">
-          <div className="relative mb-4 h-60 w-60 text-muted-foreground"
-          aria-hidden='true'
-          >
-            <Image src="/hippo-empty-cart.png" alt="empty cart" fill />
+        ) : (
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div
+              className="relative mb-4 h-60 w-60 text-muted-foreground"
+              aria-hidden="true"
+            >
+              <Image src="/hippo-empty-cart.png" alt="empty cart" fill />
+            </div>
+            <div className="text-xl font-semibold">Your cart is empty</div>
+            <SheetFooter>
+              <SheetTrigger asChild>
+                <Link
+                  href="/products"
+                  className={buttonVariants({
+                    variant: "link",
+                    size: "sm",
+                    className: "text-sm text-muted-foreground",
+                  })}
+                >
+                  Add Items to your cart to checkout
+                </Link>
+              </SheetTrigger>
+            </SheetFooter>
           </div>
-          <div className="text-xl font-semibold">Your cart is empty</div>
-          <SheetTrigger asChild>
-              <Link href="/products" className={buttonVariants({
-                variant: "link",
-                size: 'sm',
-                className: "text-sm text-muted-foreground"
-              })}>
-                Add Items to your cart to checkout
-              </Link>
-          </SheetTrigger>
-        </div>)}
+        )}
       </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
